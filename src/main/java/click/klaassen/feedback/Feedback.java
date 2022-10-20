5a42a66b066554cf9472813d5ba9c0653ed7e135
Marco Klaassen <mklaasse@redhat.com>
2022-10-20 11:40:51 +0200

    add logs

--- src/main/java/click/klaassen/feedback/Feedback.java
+++ src/main/java/click/klaassen/feedback/Feedback.java
@@ -3,10 +3,12 @@ package click.klaassen.feedback;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@ToString
 public class Feedback {
     public Integer rating;
 }
--- src/main/java/click/klaassen/feedback/FeedbackResource.java
+++ src/main/java/click/klaassen/feedback/FeedbackResource.java
@@ -1,5 +1,7 @@
 package click.klaassen.feedback;
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -7,12 +9,14 @@ import javax.ws.rs.core.MediaType;
 @Path("/feedback")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
+@Slf4j
 public class FeedbackResource {
 
     @Inject FeedbackService service;
 
     @POST
     public Feedback add(Feedback feedback) {
+        log.info("Feedback form got rating: {}", feedback);
         service.publishFeedback(feedback.getRating());
         return feedback;
     }
